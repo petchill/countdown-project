d91a7e77d6ea470475350bba6e848c93bfc6e77a
songpondev <songpon111333@gmail.com>
2020-01-20 09:48:37 +0700

    create api sever

---
+++ countdown-project/src/components/loginPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>
+            Hello this is login page
+        </h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'loginPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
